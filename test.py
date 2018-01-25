8b36ee63d000b2e05ee965e22e65f3ffdf9f6df6
juhbon <shifmaster13@gmail.com>
2018-01-26 00:48:31 +0200

    Added test.py with plot of sin and cos.

---
+++ test.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from math import sin,cos
+
+x = [i for i in range(0,1000)]
+y = [sin(x) for x in x]
+z = [cos(x) for x in x]
+
+plt.plot(x,y)
+plt.show()
+plt.plot(x,z)
+plt.show()
